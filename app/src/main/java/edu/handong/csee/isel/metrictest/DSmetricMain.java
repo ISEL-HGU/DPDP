32ac66bf5633a386e6345d3d5f42307d23ff877b
SUJIN YANG <yangmit0524@gmail.com>
2022-03-09 15:32:52 +0900

    Init the code of developer scattering metric and add the refactoring miner lib

---
+++ app/src/main/java/edu/handong/csee/isel/metrictest/DSmetricMain.java
@@ -0,0 +1,18 @@
+package edu.handong.csee.isel.metrictest;
+
+public class DSmetricMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String input = args[0]; //project_label.csv
+		String projectName = setProjectName(input);
+		
+		
+	}
+
+	private static String setProjectName(String input) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
