6b857f6764cdb82aac05ff7bc2312181b9b1d62d
SUJIN YANG <yangmit0524@gmail.com>
2022-03-20 14:06:26 +0900

    Make TestClass for testing RMiner

---
+++ app/src/main/java/edu/handong/csee/isel/metrictest/TestClass.java
@@ -0,0 +1,15 @@
+package edu.handong.csee.isel.metrictest;
+
+public class TestClass {
+	String testClass;
+	TestClass(){
+		this.testClass = null;
+	}
+	protected String getTestClass() {
+		return testClass;
+	}
+	protected void setTestClass(String testClass) {
+		this.testClass = testClass;
+	}
+	
+}
